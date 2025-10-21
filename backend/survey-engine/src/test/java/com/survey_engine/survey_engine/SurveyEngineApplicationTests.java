28c87110ec52a58cea2d1375c590edf7dac24672
K-Musembi <kevinmusembi.m@gmail.com>
2025-10-21 21:25:54 +0300

    Remove testcontainers, curtail repository tests to enable app dev testing

--- backend/survey-engine/src/test/java/com/survey_engine/survey_engine/SurveyEngineApplicationTests.java
+++ backend/survey-engine/src/test/java/com/survey_engine/survey_engine/SurveyEngineApplicationTests.java
@@ -2,10 +2,8 @@ package com.survey_engine.survey_engine;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 @TestPropertySource(properties = {
     "africastalking.api.apiKey=test_api_key",
